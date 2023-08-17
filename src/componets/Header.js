7ce5cef93e788d0446077a3063ad85ca1566c690
Fabiano Martins de Sales <fabianomartinsdev@gmail.com>
2023-08-17 17:27:08 -0300

    Second commit to deploy

---
+++ src/componets/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <a href="/">Maps</a>
+        <a href="/">About</a>
+        <a href="/">Images</a>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
